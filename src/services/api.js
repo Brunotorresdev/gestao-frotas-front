30a57f57144327425057c3105786ce19b8bab93a
Brunotorresdev <brunotorresreal@gmail.com>
2024-12-18 20:00:59 -0300

    ajustes aguardar axios

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_API,
-  timeout: 8000,
+  timeout: 12000,
   headers: { "X-Custom-Header": "foobar" },
 });
